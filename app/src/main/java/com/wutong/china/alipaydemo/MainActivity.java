98dd7fcc846422b8a2894cff11f268c11af905a0
adongbala <ad18530800653@163.com>
2017-05-17 09:42:10 +0800

    first commit

---
+++ app/src/main/java/com/wutong/china/alipaydemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wutong.china.alipaydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected Toolbar toolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        toolBar = (Toolbar) findViewById(R.id.tool_bar);
+    }
+}
